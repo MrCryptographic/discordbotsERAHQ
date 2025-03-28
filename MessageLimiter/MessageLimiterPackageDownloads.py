d54c1908e9ddb697cb4052e8ceaedfcbf45e8b78
EchoesRealmArcitect <148715187+MrCryptographic@users.noreply.github.com>
2025-03-28 17:08:53 -0400

    Create MessageLimiterPackageDownloads.py

---
+++ MessageLimiter/MessageLimiterPackageDownloads.py
@@ -0,0 +1,4 @@
+# Run this in your console with pip installed. the pip is included when you install Python.
+# Discord is needed to be installed on the system (download it by the official discord download page and select your OS) for this to work.
+
+pip install discord
